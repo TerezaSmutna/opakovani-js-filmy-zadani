22a738f38cf83caad93f2566f557f7c31cc111e4
Tereza Smutna <terka.kop@seznam.cz>
2023-02-08 14:06:04 +0100

    genres as array

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ let novaData = "";
 
 const Movie = (props) => {
   const { title, posterUrl, genres, year, url } = props;
-
+  let genresArray = genres.map((item) => `<span class="movie__genre">${item}</span>`)
   return `
   <div class="movie">
   <a href="${url}"><img class="movie__img" src="${posterUrl}" alt="${title}"></a>
   <h2 class="movie__title">${title}</h2>
   <p class="movie__year">${year}</p>
-  <p class="movie__genre">${genres}</p>
-</div>
+  ${genresArray}
+  </div>
   `
 }
+
 
 const renderMovies = () => {
   fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
